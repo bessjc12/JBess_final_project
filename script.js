5605b31deca1ad1155fa2fc2d9c597098df21e82
Chris Given <cmgiven@gmail.com>
2016-08-09 15:21:37 -0400

    Some fixes to get things working
    * Looks like you still want to geographic data file. Added that back in.
    * Take care when deleting or moving stuff that everything every curly brace is closed.
    * Remember that Javascript is case-sensistive.
    
    You'll probably want to make sure that you're copying over everything you need on line 176; currently it's just `value_2016`.

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 d3.queue()
   .defer(d3.json,'data/Access_Elec_HAP_CO2.json')
+  .defer(d3.json,'data/countries.json')
   .awaitAll(function (error, results) {
     if (error) { throw error; }
-    
+
     scatter = new DirectedScatterPlot(results[0]);
     scatter.update(results[0]);
 
-    map = new Choropleth(results[0]);
+    map = new Choropleth(results[0], results[1]);
 
     d3.select('#restart').on('click', function () {
 
@@ -26,7 +27,7 @@ var width = 625 - margin.left - margin.right;
 var height = 625 - margin.top - margin.bottom;
 
 function DirectedScatterPlot(data) {
-    
+
     var chart = this;
 
     chart.SVG = d3.select("#chart1")
@@ -111,24 +112,24 @@ DirectedScatterPlot.prototype.update = function (data) {
         .attr("class", "id")
         .attr("x", function(d){ return chart.xScale(d.ACCESS_2010) })
         .attr("y", function(d){ return chart.yScale(d.CO2KWHd1_2016) })
-        //.attr("dx", function(d){ 
+        //.attr("dx", function(d){
             //if (d.id <= 2010){ return 10 }
             //else if (d.id < 2010) { return 2}
             //else if (d.id < 1995) { return 10}
-            
+
         //})
-        //.attr("dy", function(d){ 
+        //.attr("dy", function(d){
             //if (d.year <= 2010){ return 3 }
             //else if (d.year < 2010) { return -10 }
             //else if (d.year < 1995) { return 5 }
-            
+
         //})
         .text(function(d){ return d.id })
         .attr("opacity",10)
         .transition()
         .delay(function (d,i){ return (i * 50) })
         .duration(2000)
-        .ease(d3.easePoly.exponent(3)) 
+        .ease(d3.easePoly.exponent(3))
         // Many more eases here: https://github.com/d3/d3/blob/master/API.md#easings-d3-ease
         .attr("opacity",1);
 
@@ -137,9 +138,9 @@ DirectedScatterPlot.prototype.update = function (data) {
         //.x(function(d) { return chart.xScale(d.ACCESS_2010); })
         //.y(function(d) { return chart.yScale(d.CO2KWHd1_2016); })
         //.curve(d3.curveCatmullRom.alpha(0.7));
-        
 
-    // Append a new path to the svg, using .datum() since we are binding all of our data to one new path element. We also pass the line variable to the "d" attribute. 
+
+    // Append a new path to the svg, using .datum() since we are binding all of our data to one new path element. We also pass the line variable to the "d" attribute.
     chart.svg.append("path")
         .datum(full)
         .attr("class", "line")
@@ -148,7 +149,7 @@ DirectedScatterPlot.prototype.update = function (data) {
         .transition().delay(2000).duration(1000).on("end", function(){map.update(); })
         .style("opacity", 1);
 
-};	
+};
 
 function Choropleth(change, countries){
 
@@ -164,26 +165,28 @@ function Choropleth(change, countries){
     // Data merge:
     for (var i = 0; i < change.length; i++) {
 
-        var dataCountry = change[i].Country;
+        var dataCountry = change[i].country;
         var value_2016 = change[i].CO2KWHd1_2016;
-    
+
         // Find the corresponding country inside the GeoJSON
         for (var j = 0; j < countries.features.length; j++)  {
             var jsonCountry = countries.features[j].properties.name;
 
             if (dataCountry == jsonCountry) {
-            countries.features[j].properties.value_2016 = value_2016; 
-            //countries.features[j].properties.value_2010 = value_2010; 
+            countries.features[j].properties.value_2016 = value_2016;
+            //countries.features[j].properties.value_2010 = value_2010;
             break;
             };
         };
+    };
     chart.countries = countries;
 
 };
+
 Choropleth.prototype.update = function () {
 
     var chart = this;
-    // Interrupt ongoing transitions:  
+    // Interrupt ongoing transitions:
     chart.svg.selectAll("*").interrupt();
 
     chart.colorScale = d3.scaleLinear()
@@ -193,7 +196,7 @@ Choropleth.prototype.update = function () {
     // First create a map projection and specify some options:
     var projection = d3.geoEquirectangular()
        .translate([width/2, height/2])// Places the map in the center of the SVG
-       .scale([width * 1.5]); // Scales the size of the map
+       .scale([width * 0.2]); // Scales the size of the map
 
     // Then pass this projection to d3.geoPath() - which is analagous to d3.line()
     var projectionPath = d3.geoPath().projection(projection);
@@ -206,21 +209,11 @@ Choropleth.prototype.update = function () {
         .attr("class", "map")
         .attr("d", projectionPath)
         .attr("stroke", "black")
-        .style("fill", function(d){ 
+        .style("fill", function(d){
             return chart.colorScale(d.properties.value_2016);
         })
         .transition().duration(5000)
         .style("fill", function(d){
             return chart.colorScale(d.properties.value_2016);
         })
-
-    };
 };
-
-
-
-
-
-
-
-
